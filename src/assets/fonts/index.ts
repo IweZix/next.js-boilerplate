4cf9f5a09bc051f2e85a76d0a51d977cf0a43c10
LucaN <lucanclv2@gmail.com>
2025-03-31 18:46:12 +0200

    initialise default style

---
+++ src/assets/fonts/index.ts
@@ -0,0 +1,63 @@
+export interface Font {
+    fontFamily: string;
+    fontWeight?:
+      | "100"
+      | "200"
+      | "300"
+      | "400"
+      | "500"
+      | "600"
+      | "700"
+      | "800"
+      | "900";
+    fontSize: number;
+    letterSpacing?: number;
+    lineHeight?: number;
+  }
+  
+  export const FONT_FAMILY = {
+      AlumniSansRegular: 'AlumniSans-Regular',
+      AlumniSansBold: 'AlumniSans-Bold',
+      AlumniSansLight: 'AlumniSans-Light',
+      AlumniSansSemiBold: 'AlumniSans-SemiBold',
+      SpaceGroteskRegular: 'SpaceGrotesk-Regular',
+      SpaceGroteskBold: 'SpaceGrotesk-Bold',
+      SpaceGroteskLight: 'SpaceGrotesk-Light',
+      SpaceGroteskSemiBold: 'SpaceGrotesk-SemiBold',
+    };
+  
+  export interface Fonts {
+      T1: {
+          T10px : {
+              Regular: Font;
+              Bold: Font;
+              SemiBold: Font;
+              Light: Font;
+          }
+      }
+  }
+  
+  const FONTS: Fonts = {
+      T1: {
+          T10px: {
+            Regular: {
+              fontFamily: FONT_FAMILY.SpaceGroteskRegular,
+              fontSize: 10,
+            },
+            Bold: {
+              fontFamily: FONT_FAMILY.SpaceGroteskBold,
+              fontSize: 10,
+            },
+            SemiBold: {
+              fontFamily: FONT_FAMILY.SpaceGroteskSemiBold,
+              fontSize: 10,
+            },
+            Light: {
+              fontFamily: FONT_FAMILY.SpaceGroteskLight,
+              fontSize: 10,
+            },
+          },
+      },
+  }
+  
+  export default FONTS;
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, VStack } from "@chakra-ui/react";
+
+const homePage = () => {
+  return (
+    <VStack>
+      <Text>This is the home page</Text>
+    </VStack>
+  );
+};
+
+export default homePage;
