54ebb76369ba9807912f667a4e9c897cc69bca14
LucaN <lucanclv2@gmail.com>
2025-04-12 20:41:06 +0200

    feat: renderPageTitle

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import { Provider } from '@/components/ui/provider';
 import '@/styles/globals.css';
+import renderPageTitle from '@/utils/render';
 import type { AppProps } from 'next/app';
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    renderPageTitle('Next.js - Boilerplate');
+  });
+
   return (
     <>
       <Provider>
