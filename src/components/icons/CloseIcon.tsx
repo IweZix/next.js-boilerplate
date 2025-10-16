9614f9f0e7315828f7acf33cd0ee914bcaaed6d0
IweZix <lucanclv2@gmail.com>
2025-10-16 17:28:24 +0200

    update

--- src/components/icons/CloseIcon.tsx
+++ src/components/icons/CloseIcon.tsx
@@ -6,9 +6,16 @@ interface IconProps extends React.SVGProps<SVGSVGElement> {
 }
 
 const CloseIcon: React.FC<IconProps> = ({ size = 24, color = 'currentColor', ...props }) => (
-  <svg {...props} width={size} height={size} fill={color} width="34" height="35" viewBox="0 0 34 35"  xmlns="http://www.w3.org/2000/svg">
-  <rect x="0.347458" y="4.45406" width="5" height="42" transform="rotate(-45 0.347458 4.45406)" />
-  <rect x="30.0459" y="0.918526" width="5" height="42" transform="rotate(45 30.0459 0.918526)" />
+  <svg
+    {...props}
+    width={size}
+    height={size}
+    fill={color}
+    viewBox="0 0 34 35"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect x="0.347458" y="4.45406" width="5" height="42" transform="rotate(-45 0.347458 4.45406)" />
+    <rect x="30.0459" y="0.918526" width="5" height="42" transform="rotate(45 30.0459 0.918526)" />
   </svg>
 );
 
