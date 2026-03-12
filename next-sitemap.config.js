68dd55a147a0ba0dd79359b294cc2e9bc9c94b0a
IweZix <lucanclv2@gmail.com>
2026-03-12 20:12:17 +0100

    init: nextjs app router & shadcn/ui

--- next-sitemap.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: 'https://www.monsite.xyz', // TODO: remplace par ton URL
-  generateRobotsTxt: true, // génère automatiquement robots.txt
-  sitemapSize: 5000, // nombre maximum d’URL par fichier sitemap
-  changefreq: 'weekly',
-  priority: 0.7,
-};
