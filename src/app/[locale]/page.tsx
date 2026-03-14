dc8ea4e545239d0ab46f2eda5296ede03f80f8ee
IweZix <lucanclv2@gmail.com>
2026-03-14 18:38:26 +0100

    feat: initial commit

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 import { useTranslations } from 'next-intl';
 import { tKeys } from '@/localization/tKeys';
-import { Button } from '@/components/ui/button';
 
 export default function Home() {
   const t = useTranslations();
@@ -17,7 +16,6 @@ export default function Home() {
       <h1 className="text-3xl font-bold underline">
         {t(tKeys.homepage.title)}
       </h1>
-      <Button variant="outline">Button</Button>
     </div>
   );
 }
