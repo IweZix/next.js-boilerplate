e147c461d64772220340ca9ab882e80a77dfaa42
IweZix <lucanclv2@gmail.com>
2026-03-14 16:37:54 +0100

    feat: SEO

---
+++ src/app/[locale]/about/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function About() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">about</h1>
+    </div>
+  );
+}
--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,10 +1,28 @@
 import './globals.css';
+import { Metadata } from 'next';
 import { notFound } from 'next/navigation';
 import { NextIntlClientProvider } from 'next-intl';
 import { getMessages } from 'next-intl/server';
 import EnvBanner from '@/components/core/banners/env-banner';
 import { routing } from '@/localization/routing';
 import type { Locale } from '@/types/Locale';
+
+/**
+ * Metadata for each page, can be overridden by page-specific metadata (e.g., in page.tsx)
+ */
+export const metadata: Metadata = {
+  metadataBase: new URL('https://tonsite.be'), // base url
+  title: {
+    default: 'Website', // default title
+    template: '%s – Website', // template for page titles
+  },
+  description: 'Default description for the website',
+  openGraph: {
+    siteName: 'Website',
+    locale: 'fr_BE',
+    type: 'website',
+  },
+};
 
 export function generateStaticParams() {
   return routing.locales.map((locale) => ({ locale }));
@@ -25,7 +43,7 @@ export default async function RootLayout({
   const messages = await getMessages();
 
   return (
-    <html lang="en">
+    <html lang="fr">
       <body>
         <NextIntlClientProvider messages={messages}>
           <EnvBanner />
